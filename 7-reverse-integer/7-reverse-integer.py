d6f394240cd8a0787f3799b24583ce06337e860e
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-05-04 21:31:33 +0800

    Time: 63 ms (13.33%), Space: 13.8 MB (97.42%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -1,17 +1,24 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        
         if x < 0:
             sign = -1
         else:
             sign = 1
-                   
-        if abs(x) < 10:
-            return x
         
-        else:
-            x = abs(x)
-            digits = len(str(x))
-            result = (self.reverse(x//10) + (x % 10) * (10 ** (digits - 1))) * sign
-            return result if abs(result) < pow(2, 31) - 1 else 0
+        # recursion method           
+#         if abs(x) < 10:
+#             return x
+        
+#         else:
+#             x = abs(x)
+#             digits = len(str(x))
+#             result = (self.reverse(x//10) + (x % 10) * (10 ** (digits - 1))) * sign
+        # reverse list
+        s = str(abs(x))
+        re_s = s[::-1]
+        result = int(re_s) * sign    
+        
+        return result if abs(result) < pow(2, 31) - 1 else 0
 
         
