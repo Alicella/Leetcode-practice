e3ce5d97ab162a0bd9193bb580d49c88075b0483
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-06-04 12:33:30 +0800

    Time: 46 ms (87.85%), Space: 17 MB (13.97%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -6,33 +6,54 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-#         def collect_node(root):
+        output =[]
+        self.inorder(root, output)
+        
+        for i in range(1, len(output)):
+            if output[i-1] >= output[i]:
+                return False
+        
+        return True
+    
+    # Time complexity of inorder traversal is O(n)
+    # Fun fact: Inorder traversal leads to a sorted array if it is 
+    # a Valid Binary Search. Tree.
+    def inorder(self, root, output):
+        if root is None:
+            return
+        
+        self.inorder(root.left, output)
+        output.append(root.val)
+        self.inorder(root.right, output)
+        
+        
+        
+        
+#         def in_tr(root):        # !!!This is simply inorder traversal!!!
 #             # """ Turn a tree into a list of nodes values"""           
 #             if not root:
 #                 return []
-#             elif not root.left and not root.right:
-#                 return [root.val]
 #             else:
-#                 return collect_node(root.left) + [root.val] + collect_node(root.right)
+#                 return in_tr(root.left) + [root.val] + in_tr(root.right)
 
-#         if not root:
-#             return True
-#         elif root.left and max(collect_node(root.left)) >= root.val:
-#             print('root, root.left', root.val, root.left.val)
-#             return False
-#         elif root.right and min(collect_node(root.right)) <= root.val:
-#             print('root, root.rightt', root.val, root.right.val)
-#             return False
+#         for i in range(1,len(in_tr(root))):
+#             if in_tr(root)[i] <= in_tr(root)[i - 1]:
+#                 return False
+#         return True
+            
+            
+            
+#         
 #         else:
 #             return self.isValidBST(root.left) and self.isValidBST(root.right)
             
-        def validate(root, low = -math.inf, high = math.inf):
-            if not root:
-                return True
-            elif root.val <= low or root.val >= high:
-                return False
+#         def validate(root, low = -math.inf, high = math.inf):
+#             if not root:
+#                 return True
+#             elif root.val <= low or root.val >= high:
+#                 return False
             
-            return (validate(root.left, low, root.val)) and (validate(root.right, root.val, high))
+#             return (validate(root.left, low, root.val)) and (validate(root.right, root.val, high))    
     
-        return validate(root)
+#         return validate(root)
         
