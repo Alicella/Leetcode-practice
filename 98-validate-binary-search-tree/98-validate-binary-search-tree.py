7346593c84d40a54d08769dff3c95711612e29b0
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-06-04 11:55:03 +0800

    Time: 59 ms (55.19%), Space: 16.5 MB (77.26%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -1,35 +1,38 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-    def is_leaf(self):
-        if not self.left and not self.right:
-            return True
-        return False
-    
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def collect_node(root):
-            """ Turn a tree into a list of nodes values"""
+#         def collect_node(root):
+#             # """ Turn a tree into a list of nodes values"""           
+#             if not root:
+#                 return []
+#             elif not root.left and not root.right:
+#                 return [root.val]
+#             else:
+#                 return collect_node(root.left) + [root.val] + collect_node(root.right)
+
+#         if not root:
+#             return True
+#         elif root.left and max(collect_node(root.left)) >= root.val:
+#             print('root, root.left', root.val, root.left.val)
+#             return False
+#         elif root.right and min(collect_node(root.right)) <= root.val:
+#             print('root, root.rightt', root.val, root.right.val)
+#             return False
+#         else:
+#             return self.isValidBST(root.left) and self.isValidBST(root.right)
+            
+        def validate(root, low = -math.inf, high = math.inf):
             if not root:
-                return []
-            elif not root.left and not root.right:
-                return [root.val]
-            else:
-                return collect_node(root.left) + [root.val] + collect_node(root.right)
+                return True
+            elif root.val <= low or root.val >= high:
+                return False
             
-            
-        if not root:
-            return True
-        elif root.left and max(collect_node(root.left)) >= root.val:
-            print('root, root.left', root.val, root.left.val)
-            return False
-        elif root.right and min(collect_node(root.right)) <= root.val:
-            print('root, root.rightt', root.val, root.right.val)
-            return False
-        else:
-            return self.isValidBST(root.left) and self.isValidBST(root.right)
-        
+            return (validate(root.left, low, root.val)) and (validate(root.right, root.val, high))
+    
+        return validate(root)
         
