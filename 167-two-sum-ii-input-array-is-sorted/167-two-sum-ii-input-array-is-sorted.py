2bb068a0586110dcb91ded8f680cafe92f175ab2
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-06-09 17:15:29 +0800

    Time: 253 ms (14.41%), Space: 14.9 MB (89.05%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,12 +1,15 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         # loop through the list, for each element, find if target-element is in the list
-        res = []
+        res = {}
+        
         for i in range(len(numbers)):
             x = target - numbers[i]
-            if x in numbers[i+1:]:
-                print(x, i+1)
-                res.append(i + 1)
-                res.append(numbers[i+1:].index(x) + i + 2)
-                return res
+            if x not in res:
+                res[numbers[i]] = i + 1
+            else:
+                return [res[x], i+1]
+        
+        
+            
                 
