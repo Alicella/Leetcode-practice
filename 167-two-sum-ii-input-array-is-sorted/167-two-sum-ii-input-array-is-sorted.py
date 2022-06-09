3bc37053afc2d0790af7156df763e89d1020020e
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-06-09 17:21:31 +0800

    Time: 138 ms (81.89%), Space: 14.9 MB (89.05%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -3,17 +3,27 @@ class Solution:
         # loop through the list, for each element, find if target-element is in the list
         res = {}
         
-        for i in range(len(numbers)):
-            x = target - numbers[i]
-            # if x not in res:
-            #     res[numbers[i]] = i + 1
-            # else:
-            #     return [res[x], i+1]
+#         for i in range(len(numbers)):
+#             x = target - numbers[i]
+#             # if x not in res:
+#             #     res[numbers[i]] = i + 1
+#             # else:
+#             #     return [res[x], i+1]
             
-            for i, num in enumerate(numbers):
-                if target - num in res:
-                    return [res[target-num]+1, i+1]
-                res[num] = i
+        # for i, num in enumerate(numbers):
+        #     if target - num in res:
+        #         return [res[target-num]+1, i+1]
+        #     res[num] = i
+        
+        l, r = 0, len(numbers)-1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return [l+1, r+1]
+            elif s < target:
+                l += 1
+            else:
+                r -= 1
         
         
             
