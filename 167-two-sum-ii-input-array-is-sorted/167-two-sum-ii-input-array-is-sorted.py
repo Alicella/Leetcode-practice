2c1454159b1e6e93de430aa1346ace67e5ef6d94
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-06-09 17:30:59 +0800

    Time: 135 ms (86.78%), Space: 14.9 MB (41.52%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -16,15 +16,17 @@ class Solution:
         #     res[num] = i
         
         l, r = 0, len(numbers)-1
-        while l < r:
-            s = numbers[l] + numbers[r]
-            if s == target:
-                return [l+1, r+1]
-            elif s < target:
+        
+        x = numbers[l] + numbers[r]
+        
+        while x != target:
+            if x < target:
                 l += 1
             else:
                 r -= 1
-        
+            x = numbers[l] + numbers[r]
+            
+        return [l+1, r+1]
         
             
                 
