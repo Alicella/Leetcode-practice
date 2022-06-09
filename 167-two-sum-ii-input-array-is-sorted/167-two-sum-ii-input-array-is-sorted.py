01acc9444606c009271207d9de538f77ca60b5d7
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-06-09 17:19:48 +0800

    Time: 185 ms (45.34%), Space: 15.2 MB (7.13%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -5,10 +5,15 @@ class Solution:
         
         for i in range(len(numbers)):
             x = target - numbers[i]
-            if x not in res:
-                res[numbers[i]] = i + 1
-            else:
-                return [res[x], i+1]
+            # if x not in res:
+            #     res[numbers[i]] = i + 1
+            # else:
+            #     return [res[x], i+1]
+            
+            for i, num in enumerate(numbers):
+                if target - num in res:
+                    return [res[target-num]+1, i+1]
+                res[num] = i
         
         
             
