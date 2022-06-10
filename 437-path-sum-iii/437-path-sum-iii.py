1ae0e5fad35de443e75762c418cdb2633569d377
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-06-10 18:37:19 +0800

    Time: 57 ms (87.26%), Space: 15.4 MB (29.58%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.py
+++ 437-path-sum-iii/437-path-sum-iii.py
@@ -7,21 +7,48 @@
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         
-        # intuitive, O(n2)
-        def pathSum_r(root, targetSum):
-            if not root:
-                return 0
-            res = 0
-            if root.val == targetSum: res += 1
+        # use hash table/dictionary
+        # two tutorials: https://www.youtube.com/watch?v=VDTZiggKlAE      https://www.youtube.com/watch?v=KvLzxCHnu_A
+        count = 0
+        prefixSum = defaultdict(int) 
+        prefixSum[0] = 1
+        
+        def dfs(root, curSum):
+            nonlocal count                  # why prefixSum doesn't need to be nonlocal
+            if not root: return
+            
+            curSum += root.val
+            # print(curSum)
+            if curSum - targetSum in prefixSum:
+                count += prefixSum[curSum - targetSum]
                 
-            res += pathSum_r(root.left, targetSum - root.val)
-            res += pathSum_r(root.right, targetSum - root.val)
-            return res
+            prefixSum[curSum] += 1          
+            # print(prefixSum)  
+            
+            dfs(root.left, curSum)
+            dfs(root.right, curSum)
+            
+            prefixSum[curSum] -= 1
         
-        if not root:
-            return 0
+        dfs(root, 0)
         
-        return self.pathSum(root.left, targetSum) + self.pathSum(root.right, targetSum) + pathSum_r(root, targetSum)
+        return count
+   
+        
+        
+        
+        
+        # intuitive, O(n2)
+#         def pathSum_r(root, targetSum):
+#             if not root: return 0
+#             res = 0
+#             if root.val == targetSum: res += 1
+#             res += pathSum_r(root.left, targetSum - root.val)
+#             res += pathSum_r(root.right, targetSum - root.val)
+#             return res
+        
+#         if not root: return 0
+#         return self.pathSum(root.left, targetSum) + self.pathSum(root.right, targetSum) + pathSum_r(root, targetSum)
         
         
         
