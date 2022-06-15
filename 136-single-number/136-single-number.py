1bdff9514d39da995cb4c9450583fc763bd7f759
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-06-15 09:44:50 +0800

    Time: 1864 ms (12.59%), Space: 16.7 MB (49.76%) - LeetHub

---
+++ 136-single-number/136-single-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # loop over the list, create a new list to store numbers met
+        # if met a repeated num, delete the num in the list (n/2 space?)
+        
+        tmp = []
+        for i in nums:
+            if i not in tmp:
+                tmp.append(i)
+            else:
+                tmp.remove(i)
+        
+        return tmp[0]
+                
