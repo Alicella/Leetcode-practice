cc92e01e36d8b08168d4d4453e3d83faa8640e62
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-06-17 18:38:48 +0800

    Time: 168 ms (95.09%), Space: 15.5 MB (85.42%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,12 +1,23 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dic = {}
-        n = len(nums)
+        res, count = 0, 0
+        
         for num in nums:
-            # if num not in dic:
-            #     dic[num] = 1
-            # else:
-            #     dic[num] += 1
-            dic[num] = dic.get(num, 0) + 1
-            if dic[num] > n//2:
-                return num
+            if count == 0:
+                res = num
+            if num == res:
+                count += 1
+            else:
+                count -= 1
+        return res
+        
+        
+        
+        
+        
+        # dic = {}
+        # n = len(nums)
+        # for num in nums:
+        #     dic[num] = dic.get(num, 0) + 1
+        #     if dic[num] > n//2:
+        #         return num
