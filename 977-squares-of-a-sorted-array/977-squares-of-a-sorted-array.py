9c3391815105425a7664752718a67faa7c9eb5ee
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-09-21 09:54:25 -0700

    Time: 240 ms (89.41%), Space: 16.3 MB (49.29%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -1,13 +1,55 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         
-        # Thinking:
+        # intuitive solutoin:
         # build a list of squared nums, then sort the new list
         
-        square = nums.copy()
-        for i in range(len(square)):
-            square[i] = nums[i] * nums[i]
+        # square = nums.copy()
+        # for i in range(len(square)):
+        #     square[i] = nums[i] * nums[i]    
+        # square.sort()         # O(nlogn)!!!!!!!!!!!!!!!!
+        # return square
         
-        square.sort()
+        # Two pointers: 
+        # Abs values of nums is "V" shape, or "\" or "/" shape
+        # compare and insert the left and right most nums ^2 in order
+        # l and r move inwards---- once a time!!!
+
+            
+       
+        l = 0
+        r = len(nums) - 1
+        w = len(nums) - 1    # the writing pointer
+        square = [0] * len(nums)
+        
+        while w >= 0:
+            if abs(nums[l]) > abs(nums[r]):
+                square[w] = nums[l] ** 2
+                l += 1
+            else:
+                square[w] = nums[r] ** 2
+                r -= 1
+            w -= 1
         
         return square
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            
+            
