ec74efb6a0dbd92a6d4447716c46066a4ebdaa31
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-09-21 08:56:34 -0700

    Time: 70 ms (15.57%), Space: 13.8 MB (98.89%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -4,13 +4,23 @@ class Solution:
         # for each char in s, if can't find in t, -> False
         # search t for the next char after the position where the previous char is found
         
-        cur_position = 0
-        for char in s:
-            index = t.find(char, cur_position)
+#         cur_position = 0
+#         for char in s:
+#             index = t.find(char, cur_position)
             
-            if index == -1:
-                return False
-            else:
-                cur_position = index + 1
+#             if index == -1:
+#                 return False
+#             else:
+#                 cur_position = index + 1
         
-        return True
+#         return True
+        
+        # ANOTHER Two pointers solution
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        
+        return i == len(s)
+        
