f87e1ab644f5aaf811acb91230c2c36a89f9d793
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-09-21 16:41:51 -0700

    Time: 1248 ms (23.45%), Space: 25.5 MB (6.99%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -4,11 +4,39 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        # NO.1 use list insert method:
+        # No.3 rotate left by n-k steps?
+
+        n = len(nums)
+        if n > k:
+            for i in range(n - k):
+                nums.append(nums.pop(0))
+        else:
+           # in the first k // n times,
+           # rotating just doesn't make change to nums.
+           # actual_rotation = k % n
+            for i in range(n - k % n):
+                nums.append(nums.pop(0))
+        
+        
+        
+        # No.2 use collection.deque... doesn't work
+        # created a copy to the reference?
+#         nums = collections.deque(nums)
+#         for i in range(k):
+#             rotated_el = nums.pop()
+#             nums.appendleft(rotated_el)
+        
+#         nums = list(nums)
+        
+        
+        
+        # No.1 use list insert method:
         # O(n * k)
       
-        for i in range(-1, -k - 1, -1):
-            rotated_el = nums.pop()
-            nums.insert(0, rotated_el)
+        # for i in range(-1, -k - 1, -1):
+        #     rotated_el = nums.pop()
+        #     nums.insert(0, rotated_el)
         
+        
+    
         
