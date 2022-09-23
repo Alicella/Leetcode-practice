a85bac76e8b619ae946459fcc43126cc55e66832
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-09-23 11:38:06 -0700

    Time: 72 ms (12.30%), Space: 13.8 MB (66.87%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -4,27 +4,40 @@ class Solution:
         # palindrome needs 0/1 unique letter and 
         # 2n identicle letter pairs
         # the length = 2n or 2n + 1
-        # solve using hash table/dictionary?
+        # solve using hash table/dictionary
         
-        counts = {}
-        for char in s:
-            if char in counts:
-                counts[char] += 1
-            else:
-                counts[char] = 1
+#         counts = {}
+#         for char in s:
+#             if char in counts:
+#                 counts[char] += 1
+#             else:
+#                 counts[char] = 1
         
-        # a single letter can be a palindrome
-        length = 0
-        odd_count_exist = False
+#         length = 0
+#         odd_count_exist = False
         
-        for count in counts.values():
-            if count % 2 == 0:
-                length += count
-            else:
-                length += count - 1
-                odd_count_exist = True
+#         for count in counts.values():
+#             if count % 2 == 0:
+#                 length += count
+#             else:
+#                 length += count - 1
+#                 odd_count_exist = True
         
-        if odd_count_exist:
-            length += 1
+#         if odd_count_exist:
+#             length += 1
             
-        return length
+#         return length
+
+
+        # use set()
+        pairs = 0
+        unpaired_chars = set()
+        
+        for char in s:
+            if char in unpaired_chars:
+                pairs += 1
+                unpaired_chars.remove(char)
+            else:
+                unpaired_chars.add(char)
+        
+        return pairs * 2 + 1 if unpaired_chars else pairs * 2
