cc16448265faa115551a35ade09a264158ead353
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-09-22 20:15:38 -0700

    Time: 59 ms (26.54%), Space: 13.9 MB (55.55%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -9,17 +9,33 @@ class Solution:
         # initialize number of nodes = 1
         # initialize mid pointer position = 1
         # initialize mid and cur pointers point to head
-        node_num = 1
-        mid_pos = 1
-        mid = head
-        cur = head
+#         node_num = 1
+#         mid_pos = 1
+#         mid = cur = head
         
-        while cur.next != None:
-            cur = cur.next
-            node_num += 1       
-            
-            while mid_pos <= node_num / 2:
-                mid = mid.next
-                mid_pos += 1
+#         while cur.next != None:
+#             cur = cur.next
+#             node_num += 1       
+#             # below is how to move the mid pointer
+#             while mid_pos <= node_num / 2:
+#                 mid = mid.next
+#                 mid_pos += 1
         
-        return mid
+#         return mid
+        
+        # simplified solution 
+        # https://leetcode.com/problems/middle-of-the-linked-list/discuss/154619/C%2B%2BJavaPython-Slow-and-Fast-Pointers
+        
+        slow = fast = head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        return slow
+        
+        
+        
+        
+    
+        
