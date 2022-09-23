3f4a9be80f5191d90f98d8f214667d3cb36827ed
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-09-23 11:18:03 -0700

    Time: 2582 ms (13.27%), Space: 25 MB (38.39%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,22 +1,35 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # to maximize profit, buy at a lowest price and sell at a highest, profit = the difference
-        crt_lowest = prices[0]   #setting the initial value brings huge runtime difference... why prices[0] is much slower than float('inf')?
-        max_profit = 0
+#         crt_lowest = prices[0]
+#         max_profit = 0
         
-        for p in prices:
-            if p < crt_lowest:
-                crt_lowest = p
-            
-            # this one is no good, no need to make such comparison or assignments. wasting runtime
-            # elif p > crt_lowest:
-            #     crt_highest = p
-            #     max_profit = max(max_profit, crt_highest - crt_lowest)    
+#         for p in prices:
+#             if p < crt_lowest:
+#                 crt_lowest = p
                 
-            if p - crt_lowest > max_profit:
-                max_profit = p - crt_lowest
+#             if p - crt_lowest > max_profit:
+#                 max_profit = p - crt_lowest
             
+#         return max_profit
+
+
+        # two pointers solution
+        l = 0
+        r = 1
+        max_profit = 0
+        while r < len(prices):
+            if prices[l] >= prices[r]:
+                l = r
+                r += 1
+            else:
+                max_profit = max(max_profit, prices[r] - prices[l])
+                r += 1
+         
         return max_profit
+        
+        
+        
     
     # if the question asks the days and prices of selling/buying info:
     
