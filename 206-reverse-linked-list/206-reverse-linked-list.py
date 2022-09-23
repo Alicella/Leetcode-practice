f17f7765c987c1a2777c9e79f066d9fc8dfaf167
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-09-22 17:37:39 -0700

    Time: 77 ms (15.11%), Space: 20.4 MB (15.63%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -4,40 +4,50 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:     
+        # iteratively, two(actually three) pointers
+#         prev = None
+#         cur = head
         
-        # iteratively, two pointers
-        prev = None
-        cur = head
-        
-        while cur != None:
-            # store the linked list part after the cur pointer
-            nxt = cur.next
+#         while cur != None:
+#             # store the linked list part after the cur pointer
+#             nxt = cur.next       
             
-            # change cur to point to the prev linked list
-            cur.next = prev
+#             # change cur to point to the prev linked list
+#             cur.next = prev           
             
-            # move the two pointers
-            prev = cur
-            cur = nxt
+#             # move the two pointers
+#             prev = cur
+#             cur = nxt
             
-        return prev
+#         return prev
+        
+        # recursively
+        
+        if not head or not head.next:
+            return head
+        
+        # leading downwards to the end of the linke list
+        reversed_head = self.reverseList(head.next)
+        
+        # reverse the second last node's direction
+        head.next.next = head;
+        head.next = None
+        
+        return reversed_head
+    
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-#         if not head or not head.next:
-#             return head
-        
-#         else:
-#             tmp = head.val
-#             head.val = head.next.val
-#             head.next.val = tmp
-#             return head
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
             
