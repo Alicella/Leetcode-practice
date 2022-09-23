cc854a123f69634ad2f12fefce4223878e225d86
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-09-22 21:10:34 -0700

    Time: 101 ms (39.92%), Space: 17.5 MB (94.20%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -7,18 +7,37 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
-        # use slow and fast pointers
-        if not head or not head.next:
-            return False
+        # a neater version:
         
-        slow = head
-        fast = head.next
+        slow = fast = head
         
-        while slow != fast:
-            if fast == None or fast.next == None:
-                return False
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+            
+            if slow == fast:
+                return True
         
-        return True
+        return False
+        
+        
+        
+        
+        
+#         if not head or not head.next:
+#             return False
+        
+#         # use slow and fast pointers
+#         slow = head
+#         fast = head.next
+        
+#         while slow != fast:
+#             # fast or fast.next is null means you've reached to 
+#             # a tail without cycle
+#             if fast == None or fast.next == None:
+#                 return False
+#             slow = slow.next
+#             fast = fast.next.next
+        
+#         return True
         
