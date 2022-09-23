b36e377203aca4fa55ba726500754bc37f2b04e8
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-09-23 11:05:13 -0700

    Time: 2113 ms (29.62%), Space: 25 MB (38.39%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # to maximize profit, buy at a lowest price and sell at a highest, profit = the difference
-        crt_lowest = float('inf')
+        crt_lowest = float('inf')   #setting the initial value brings huge runtime difference... why prices[0] is much slower than float('inf')?
         max_profit = 0
         
         for p in prices:
             if p < crt_lowest:
                 crt_lowest = p
+            
+            # this one is no good, no need to make such comparison or assignments. wasting runtime
             # elif p > crt_lowest:
             #     crt_highest = p
-            #     max_profit = max(max_profit, crt_highest - crt_lowest)      
+            #     max_profit = max(max_profit, crt_highest - crt_lowest)    
+                
             if p - crt_lowest > max_profit:
                 max_profit = p - crt_lowest
             
