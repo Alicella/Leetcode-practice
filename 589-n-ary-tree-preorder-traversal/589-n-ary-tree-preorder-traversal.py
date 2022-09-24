a43cfdce318bab895d32473f68e8a672923d5f28
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-09-24 10:21:56 -0700

    Time: 127 ms (5.06%), Space: 16 MB (82.23%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
@@ -9,17 +9,33 @@ class Node:
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:
         
+        # iterative way is to use queue/stack
+        output = []
+        if not root: return
+        q = deque([root])      
+        
+        while q:
+            cand = q.popleft()
+            output.append(cand.val)
+            
+            # if not reverse and add children in order, 
+            # will become BFS
+            for c in reversed(cand.children):
+                q.appendleft(c)
+        
+        return output
+        
+        
+        # recursive way is similar to solving binary trees problems
         output = []
         def dfs(node):
             if not node: return
             
             output.append(node.val)
-            
             for c in node.children:
                 dfs(c)
             
         dfs(root)
-
         
         return output
 
