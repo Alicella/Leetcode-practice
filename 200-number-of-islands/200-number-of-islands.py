506503aef92f8779d8fdf2971f584732b9e4c34d
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-09-30 17:03:30 -0700

    Time: 569 ms (45.13%), Space: 21.6 MB (30.63%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,8 +1,50 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
+        # https://www.youtube.com/watch?v=pV2kpPD66nE&list=RDLVpV2kpPD66nE&index=1         #  BFS
+        if not grid:
+            return 0
+        
+        rows, cols = len(grid), len(grid[0])
+        visit = set()
+        islands = 0
+        
+        def bfs(r, c):
+            q = collections.deque()
+            visit.add((r, c))       #add the visited cell as a tuple
+            q.append((r, c))
+            
+            while q:
+                row, col = q.popleft()       # if q.pop, then DFS???
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if (r in range(rows) and c in range(cols) and
+                        grid[r][c] == '1' and (r, c) not in visit):
+                        q.append((r, c))
+                        visit.add((r, c))
+            
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r, c) not in visit:
+                    bfs(r, c)
+                    islands += 1
+        
+        return islands
+                        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         # https://leetcode.com/problems/number-of-islands/discuss/56340/Python-Simple-DFS-Solution
-        # DFS:
+        # DFS:    不懂
         if not grid:
             return 0
         
