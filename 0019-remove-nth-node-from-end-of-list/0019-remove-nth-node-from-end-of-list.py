8346821be6d0ebf8742fc0f8de67769b57d596bc
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-10-28 12:03:33 -0700

    Time: 63 ms (49.66%), Space: 13.8 MB (98.11%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -6,24 +6,22 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         
-        # intuition: r - l = n - 1, when r reach the end, l is the nth node to remove
+        # intuition: r - l = n, when r reach the end node, l + 1 is the nth node to remove
         
-        pre = l = r = head
-        for i in range(n - 1):
+        l = r = head
+        for i in range(n):
             r = r.next
-         
+        
+        # if r points to null of linked list end, need to remove the first node
+        if not r:
+            return head.next
+        
         # now the distance btw l pointer and r pointer is n - 1
-        # move pre, l, r pointer until r is at the end
+        # keep moving l, r pointer until r is at the end
         while r.next:
-            pre = l
             l = l.next
             r = r.next
-        
-        if l == head:    # need to remove the head node
-            head = l.next
-        elif l == r:       # need to remove the last node, so pre points to none
-            pre.next = None                 
-        else:                 # remove the middle node that l points to
-            pre.next = l.next    
+        # remove the l + 1 node
+        l.next = l.next.next    
              
         return head
