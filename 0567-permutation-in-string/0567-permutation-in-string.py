8f02b797e724edf522edfcb0a39bc85ddc11e2ae
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-10-29 10:19:01 -0700

    Time: 203 ms (43.47%), Space: 14 MB (68.14%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,27 +1,71 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s2) < len(s1):
-            return False
+        #https://www.youtube.com/watch?v=UbyhOgBN834&ab_channel=NeetCode
+        if len(s2) < len(s1): return False
         
-        dict1 = {}
-        dict2 = {}
-        for char in s1:
-            dict1[char] = dict1.get(char, 0) + 1
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            # ord function get the ASCII value of the character
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
         
-        for l in range(len(s2) - len(s1) + 1):
-            r = l + len(s1) - 1
-            for char in s2[l:r+1]:
-                if char not in dict1:
-                    dict2 = {}
-                    break
-                else:
-                    dict2[char] = dict2.get(char, 0) + 1
-                    
-            if dict2 == dict1:
-                print(dict1)
-                print(dict2)
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+        
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
                 return True
-            else:
-                dict2 = {}
+            
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+            
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            
+            l += 1
         
-        # return False
+        return matches == 26
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #O(len(s1) * len(s2))
+#         if len(s2) < len(s1):
+#             return False
+        
+#         dict1 = {}
+#         dict2 = {}
+#         for char in s1:
+#             dict1[char] = dict1.get(char, 0) + 1
+        
+#         for l in range(len(s2) - len(s1) + 1):
+#             r = l + len(s1) - 1
+#             for char in s2[l:r+1]:
+#                 if char not in dict1:
+#                     dict2 = {}
+#                     break
+#                 else:
+#                     dict2[char] = dict2.get(char, 0) + 1
+                    
+#             if dict2 == dict1:
+#                 return True
+#             else:
+#                 dict2 = {}
+        
+#         return False
