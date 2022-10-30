c24dfaa6380c71705ab1ff9f14222799b8b97ff2
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-10-29 17:06:32 -0700

    Time: 313 ms (50.70%), Space: 16.6 MB (49.44%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,16 +1,18 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         # visited = []     seems like use a set() in this case is faster
-        visited = set()
+        # visited = set()     or don't use another data structure, but mark visited land as 0
         max_area = 0
         m, n = len(grid), len(grid[0])
         
         
         def dfs(r, c):
-            if r < 0 or r == m or c < 0 or c == n or grid[r][c] == 0 or (r, c) in visited:
+            if r < 0 or r == m or c < 0 or c == n or grid[r][c] == 0:
+            # or (r, c) in visited:
                 return 0
             
-            visited.add((r,c))
+            # visited.add((r,c))
+            grid[r][c] = 0
             return 1 + dfs(r - 1, c) + dfs(r + 1, c) + dfs(r, c - 1) + dfs(r, c + 1)     # !!!!!!!!!!
         
         # O(m*n)
