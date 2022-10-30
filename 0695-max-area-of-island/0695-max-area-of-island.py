ffb8101ee4218e1a461b376242e5aea16325c667
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-10-29 17:03:28 -0700

    Time: 459 ms (9.47%), Space: 17.8 MB (27.27%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,21 +1,22 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        visited = []
+        # visited = []     seems like use a set() in this case is faster
+        visited = set()
         max_area = 0
+        m, n = len(grid), len(grid[0])
+        
+        
+        def dfs(r, c):
+            if r < 0 or r == m or c < 0 or c == n or grid[r][c] == 0 or (r, c) in visited:
+                return 0
+            
+            visited.add((r,c))
+            return 1 + dfs(r - 1, c) + dfs(r + 1, c) + dfs(r, c - 1) + dfs(r, c + 1)     # !!!!!!!!!!
         
         # O(m*n)
-        def dfs(r, c):
-            if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] == 1 and (r, c) not in visited:
-                visited.append((r,c))
-                return 1 + dfs(r - 1, c) + dfs(r + 1, c) + dfs(r, c - 1) + dfs(r, c + 1)
-            else:
-                return 0
-          
-         
-        for r in range(len(grid)):
-            for c in range(len(grid[r])):
-                if grid[r][c] == 1 and (r, c) not in visited:
-                    max_area = max(dfs(r, c), max_area)
+        for r in range(m):
+            for c in range(n):
+                max_area = max(dfs(r, c), max_area)
     
         return max_area
         
