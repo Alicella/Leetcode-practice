f7100673b87cafb88fee238ef23749cff23a6538
Alice Dawn <87024873+Alicella@users.noreply.github.com>
2022-11-02 09:53:39 -0700

    Time: 1269 ms (66.53%), Space: 16.9 MB (75.11%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,5 +1,39 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        
+        # O(m*n) time, O(1)space
+        
+        rows, cols = len(mat), len(mat[0])
+        
+        for r in range(rows):
+            for c in range(cols):
+                if mat[r][c] > 0:
+                    top = mat[r - 1][c] if r > 0 else math.inf
+                    left = mat[r] [c - 1] if c > 0 else math.inf
+                    mat[r][c] = min(top, left) + 1
+            
+        for r in range(rows - 1, -1, -1):
+            for c in range(cols - 1, -1, -1):
+                if mat[r][c] > 0:
+                    bottom = mat[r + 1][c] if r < rows - 1 else math.inf
+                    right = mat[r][c + 1] if c < cols - 1 else math.inf
+                    mat[r][c] = min(mat[r][c], bottom + 1, right + 1)
+        
+        return mat
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # O(m * n) time and space
         rows, cols = len(mat), len(mat[0])
         DIR = [0, 1, 0, -1, 0]
         
